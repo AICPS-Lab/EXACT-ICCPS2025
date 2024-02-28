a5c9de2e951dfa4935dc1713e55ee2c81e0f11f8
David Wang <32970067+HCWDavid@users.noreply.github.com>
2024-02-28 13:46:11 -0600

    1

--- methods/transformer.py
+++ methods/transformer.py
@@ -4,18 +4,21 @@ import torch.optim as optim
 from torch.utils.data import DataLoader
 import torch
 from torch.nn.modules.transformer import TransformerEncoder, TransformerEncoderLayer
+import torch.nn.functional as F
+
+from utilities import printc
 class TransformerModel(nn.Module):
     """Container module with an encoder, a recurrent or transformer module, and a decoder."""
 
-    def __init__(self, num_classes=6, ntoken=6, ninp=64, nhead=1, nhid=256, nlayers=6, dropout=0.1, init_std=.02, activation='relu'):
+    def __init__(self, num_classes=5, in_channels=6, ninp=64, num_heads=1, embed_dims=256, num_layers=6, dropout=0.1, init_std=.02, activation='relu'):
         super(TransformerModel, self).__init__()
-        self.input_emb = nn.Linear(ntoken, ninp)
+        self.input_emb = nn.Linear(in_channels, ninp)
         self.ninp = ninp
         self.relu = nn.ReLU()
         # modulelist:
-        encoder_layer = nn.TransformerEncoderLayer(d_model=ninp, nhead=nhead, dim_feedforward=nhid, dropout=dropout, activation=activation, batch_first=True)
+        encoder_layer = nn.TransformerEncoderLayer(d_model=ninp, nhead=num_heads, dim_feedforward=embed_dims, dropout=dropout, activation=activation, batch_first=True)
         encoder_norm = nn.LayerNorm(ninp)   
-        self.transformer_encoder = TransformerEncoder(encoder_layer, nlayers, norm=encoder_norm)
+        self.transformer_encoder = TransformerEncoder(encoder_layer, num_layers, norm=encoder_norm)
         self.decoder = nn.Linear(ninp, num_classes)
         self.init_std = init_std
         # max layer:
@@ -36,3 +39,10 @@ class TransformerModel(nn.Module):
         output = self.transformer_encoder(src)
         output = self.decoder(output)
         return output
+    
+    def forward_pred(self, inputs):
+        masks = self.forward(inputs)
+        masks = masks.permute(0, 2, 1)
+        probabilities = F.softmax(masks, dim=1)
+        pred = torch.argmax(probabilities, dim=1)
+        return pred
