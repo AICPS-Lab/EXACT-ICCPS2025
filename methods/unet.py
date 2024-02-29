7bda62baad986d2332d4bc8fc7c5c06d3f405460
David Wang <32970067+HCWDavid@users.noreply.github.com>
2024-02-29 15:50:38 -0600

    update

--- methods/unet.py
+++ methods/unet.py
@@ -1,85 +1,126 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
+class conbr_block(nn.Module):
+    def __init__(self, in_layer, out_layer, kernel_size, stride, dilation):
+        super(conbr_block, self).__init__()
+
+        self.conv1 = nn.Conv1d(in_layer, out_layer, kernel_size=kernel_size, stride=stride, dilation = dilation, padding = 3, bias=True)
+        self.bn = nn.BatchNorm1d(out_layer)
+        self.relu = nn.ReLU()
+    
+    def forward(self,x):
+        x = self.conv1(x)
+        x = self.bn(x)
+        out = self.relu(x)
+        
+        return out       
+
+class se_block(nn.Module):
+    def __init__(self,in_layer, out_layer):
+        super(se_block, self).__init__()
+        
+        self.conv1 = nn.Conv1d(in_layer, out_layer//8, kernel_size=1, padding=0)
+        self.conv2 = nn.Conv1d(out_layer//8, in_layer, kernel_size=1, padding=0)
+        self.fc = nn.Linear(1,out_layer//8)
+        self.fc2 = nn.Linear(out_layer//8,out_layer)
+        self.relu = nn.ReLU()
+        self.sigmoid = nn.Sigmoid()
+    
+    def forward(self,x):
+
+        x_se = nn.functional.adaptive_avg_pool1d(x,1)
+        x_se = self.conv1(x_se)
+        x_se = self.relu(x_se)
+        x_se = self.conv2(x_se)
+        x_se = self.sigmoid(x_se)
+        
+        x_out = torch.add(x, x_se)
+        return x_out
+
+class re_block(nn.Module):
+    def __init__(self, in_layer, out_layer, kernel_size, dilation):
+        super(re_block, self).__init__()
+        
+        self.cbr1 = conbr_block(in_layer,out_layer, kernel_size, 1, dilation)
+        self.cbr2 = conbr_block(out_layer,out_layer, kernel_size, 1, dilation)
+        self.seblock = se_block(out_layer, out_layer)
+    
+    def forward(self,x):
+
+        x_re = self.cbr1(x)
+        x_re = self.cbr2(x_re)
+        x_re = self.seblock(x_re)
+        x_out = torch.add(x, x_re)
+        return x_out          
 
 class UNet(nn.Module):
-    def __init__(self, in_channels=6, embed_dims=256, num_layers=2, num_classes=5, dropout_prob=.5):
+    def __init__(self ,embed_dims=6,layer_n=5,kernel_size=3,depth=3, num_classes=5):
         super(UNet, self).__init__()
-        self.down_blocks = nn.ModuleList()
-        self.up_blocks = nn.ModuleList()
-        self.pool = nn.MaxPool1d(kernel_size=2, stride=2)
+        self.input_dim = embed_dims
+        self.layer_n = layer_n
+        self.kernel_size = kernel_size
+        self.depth = depth
         
-        # Create down blocks
-        current_channels = in_channels
-        for i in range(num_layers):
-            self.down_blocks.append(ConvBlock(current_channels, embed_dims * (2 ** i), dropout_prob))
-            current_channels = embed_dims * (2 ** i)
+        self.AvgPool1D1 = nn.AvgPool1d(embed_dims, stride=5)
+        self.AvgPool1D2 = nn.AvgPool1d(embed_dims, stride=25)
+        self.AvgPool1D3 = nn.AvgPool1d(embed_dims, stride=125)
         
-        # Bottleneck
-        self.bottleneck = ConvBlock(current_channels, current_channels * 2, dropout_prob)
-        current_channels *= 2
-        
-        # Create up blocks
-        for i in range(num_layers-1, -1, -1):
-            self.up_blocks.append(nn.ConvTranspose1d(current_channels, current_channels // 2, kernel_size=2, stride=2))
-            self.up_blocks.append(ConvBlock(current_channels, embed_dims * (2 ** i), dropout_prob))
-            current_channels = embed_dims * (2 ** i)
-        
-        # Final convolution
-        self.final_conv = nn.Conv1d(current_channels, num_classes, kernel_size=1)
+        self.layer1 = self.down_layer(self.input_dim, self.layer_n, self.kernel_size,1, 2)
+        self.layer2 = self.down_layer(self.layer_n, int(self.layer_n*2), self.kernel_size,5, 2)
+        self.layer3 = self.down_layer(int(self.layer_n*2)+int(self.input_dim), int(self.layer_n*3), self.kernel_size,5, 2)
+        self.layer4 = self.down_layer(int(self.layer_n*3)+int(self.input_dim), int(self.layer_n*4), self.kernel_size,5, 2)
+        self.layer5 = self.down_layer(int(self.layer_n*4)+int(self.input_dim), int(self.layer_n*5), self.kernel_size,4, 2)
 
-        # weight init:
-        self.init_weights()
-
+        self.cbr_up1 = conbr_block(int(self.layer_n*7), int(self.layer_n*3), self.kernel_size, 1, 1)
+        self.cbr_up2 = conbr_block(int(self.layer_n*5), int(self.layer_n*2), self.kernel_size, 1, 1)
+        self.cbr_up3 = conbr_block(int(self.layer_n*3), self.layer_n, self.kernel_size, 1, 1)
+        self.upsample = nn.Upsample(scale_factor=5, mode='nearest')
+        self.upsample1 = nn.Upsample(scale_factor=5, mode='nearest')
+        
+        self.outcov = nn.Conv1d(self.layer_n, 11, kernel_size=self.kernel_size, stride=1,padding = 3)
+    
+        
+    def down_layer(self, input_layer, out_layer, kernel, stride, depth):
+        block = []
+        block.append(conbr_block(input_layer, out_layer, kernel, stride, 1))
+        for i in range(depth):
+            block.append(re_block(out_layer,out_layer,kernel,1))
+        return nn.Sequential(*block)
+            
     def forward(self, x):
-        x = x.permute(0, 2, 1)  # (B, C, L) -> (B, L, C)
-        enc_features = []
-        for block in self.down_blocks:
-            x = block(x)
-            enc_features.append(x)
-            x = self.pool(x)
+        x = x.permute(0,2,1)
+        pool_x1 = self.AvgPool1D1(x)
+        pool_x2 = self.AvgPool1D2(x)
+        pool_x3 = self.AvgPool1D3(x)
         
-        x = self.bottleneck(x)
+        #############Encoder#####################
         
-        for i in range(0, len(self.up_blocks), 2):
-            x = self.up_blocks[i](x)
-            enc_feat = enc_features.pop()  # Get the corresponding feature from the encoder
-            # Calculate padding
-            diffY = enc_feat.size()[2] - x.size()[2]
-            x = F.pad(x, [diffY // 2, diffY - diffY // 2])
-            x = torch.cat((x, enc_feat), dim=1)  # Skip connection with dynamic padding
-            x = self.up_blocks[i+1](x)
+        out_0 = self.layer1(x)
+        print(out_0.shape)
+        out_1 = self.layer2(out_0)
         
-        x = self.final_conv(x)
-        x = x.permute(0, 2, 1)
-        return x
-    def init_weights(self):
-        for module in self.modules():
-            if isinstance(module, nn.Conv1d) or isinstance(module, nn.Linear):
-                nn.init.kaiming_normal_(module.weight)
-                nn.init.constant_(module.bias, 0)
-            elif isinstance(module, nn.BatchNorm1d):
-                nn.init.constant_(module.weight, 1)
-                nn.init.constant_(module.bias, 0)
-
-    def forward_pred(self, x):
-        masks = self.forward(x)
-        masks = masks.permute(0, 2, 1)
-        probabilities = F.softmax(masks, dim=1)
-        pred = torch.argmax(probabilities, dim=1)
-        return pred
-
-class ConvBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, dropout_prob):
-        super(ConvBlock, self).__init__()
-        self.conv1 = nn.Conv1d(in_channels, out_channels, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv1d(out_channels, out_channels, kernel_size=3, padding=1)
-        self.relu = nn.ReLU(inplace=True)
-        self.dropout = nn.Dropout(dropout_prob)
-
-    def forward(self, x):
-        x = self.relu(self.conv1(x))
-        x = self.dropout(x)
-        x = self.relu(self.conv2(x))
-        x = self.dropout(x)
-        return x
+        x = torch.cat([out_1,pool_x1],1)
+        out_2 = self.layer3(x)
+        
+        x = torch.cat([out_2,pool_x2],1)
+        x = self.layer4(x)
+        
+        #############Decoder####################
+        
+        up = self.upsample1(x)
+        up = torch.cat([up,out_2],1)
+        up = self.cbr_up1(up)
+        
+        up = self.upsample(up)
+        up = torch.cat([up,out_1],1)
+        up = self.cbr_up2(up)
+        
+        up = self.upsample(up)
+        up = torch.cat([up,out_0],1)
+        up = self.cbr_up3(up)
+        
+        out = self.outcov(up)
+        
+        #out = nn.functional.softmax(out,dim=2)
+        
+        return out
