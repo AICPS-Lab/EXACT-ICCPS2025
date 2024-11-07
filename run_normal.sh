20d4eb0ca19aa23e2358c6171a491fbc205e07f2
HCWDavid <wang584041187@gmail.com>
2024-11-06 22:28:30 -0600

    code changed

--- run_normal.sh
+++ run_normal.sh
@@ -5,7 +5,7 @@ datasets=("physiq" "mmfit" "spar")
 models=("ex" "transformer" "unet")
 
 # Loop over seeds, datasets, and models
-for seed in {0..10}; do
+for seed in {1..10}; do
   for dataset in "${datasets[@]}"; do
     for model in "${models[@]}"; do
       python main_meta_v2.py --add_side_noise --dataset "$dataset" --model "$model" --seed "$seed"
