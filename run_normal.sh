a8d4c77b1b9aa89bde39a78e9ab7f107e3d3c5aa
HCWDavid <wanghanchend@gmail.com>
2024-11-10 17:07:22 +0000

    add file

--- run_normal.sh
+++ run_normal.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 # Define datasets and models
-datasets=("mmfit")
-models=("ex" "transformer" "unet")
+datasets=("mmfit" "physiq" "spar")
+models=("transformer" "unet" "cnn" "ex" "segmenter")
 
 # Loop over seeds, datasets, and models
-for seed in {5..10}; do
+for seed in {43..44}; do  # Run four times with seeds 43, 44, 45, 46
   for dataset in "${datasets[@]}"; do
     for model in "${models[@]}"; do
-      # Check if the dataset is 'mmfit' and add --rotation_chance .5 if true
-      python main_meta_v2.py --add_side_noise --dataset "$dataset" --model "$model" --seed "$seed"
+      python main_meta_v2.py --add_side_noise --dataset "$dataset" --model "$model" --seed "$seed" --n_epochs 200
     done
   done
 done
